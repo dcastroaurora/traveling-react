ddcb21f322d2fe8bb689198459a425a8ef91d970
Diego Castro <dcastroaurora@hotmail.com>
2022-02-20 17:13:38 -0500

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from 'react';
+import TourCard from './components/TourCard';
+import SearcAppBar from './components/AppBar';
+import Container from '@mui/material/Container';
+import Grid from "@mui/material/Grid";
+import cities from "./data.json";
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <SearcAppBar />
+      <Container sx={{marginY: 5}}>
+        {
+          cities.map((city) => (
+            <>
+              <Typography
+                variant="h4"
+                component="h2"
+                marginTop={5}
+                marginBottom={3}>
+                  Top {city.name} Tours
+              </Typography>
+              <Grid container spacing={5}>
+                {
+                  city.tours.map((tour, index) => <TourCard tour={tour} key={index} />)
+                }                
+              </Grid>
+            </>
+          ))
+        }
+      </Container>
     </div>
   );
 }
